9dbf3fd25fcfb0a5795caa1d7eac1f1676b09f33
marktomarselocq <Mark.Tomarse@locq.com>
2024-08-13 21:03:21 +0800

    redis creds

--- src/modules/cache/config/cache.config.ts
+++ src/modules/cache/config/cache.config.ts
@@ -1,2 +1,2 @@
 export const EMAIL_VERIFICATION_KEY_SUFFIX = 'email-verification-duration-';
-export const EMAIL_EXPIRATION_TO_SEND_SECONDS = 30; // 60 seconds
+export const EMAIL_EXPIRATION_TO_SEND_SECONDS = 60; // 60 seconds
